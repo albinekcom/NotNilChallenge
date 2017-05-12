2e353c450c54c7bad60cc48fa60a382d69abc7c8
Albin "albinek" Sadowski <albinek@albinek.com>
2017-05-13 00:03:31 +0200

    Improve tests

--- Tests/NotNilChallengeTests/NotNilChallengeTests.swift
+++ Tests/NotNilChallengeTests/NotNilChallengeTests.swift
@@ -1,34 +1,31 @@
 import XCTest
 
 final class NotNilChallengeTests: XCTestCase {
-    
+
     let attempts = 10_000_000
-    
-    
+
+    private let value: Int? = 1
+
     func testIfLet() {
-        let value: Int? = 1
-        
         measure {
             for _ in 0..<self.attempts {
-                if let _ = value {}
+                if let _ = self.value {}
             }
         }
     }
-    
+
     func testIfNotNil() {
-        let value: Int? = 1
-        
         measure {
             for _ in 0..<self.attempts {
-                if value != nil {}
+                if self.value != nil {}
             }
         }
     }
-    
-    
+
+
     static var allTests = [
         ("testIfLet", testIfLet),
         ("testIfNotNil", testIfNotNil)
     ]
-    
+
 }
